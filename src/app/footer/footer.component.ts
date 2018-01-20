0e8d4b8a19373ede22fef3131aa147134c8d80f1
IhToN <ihton93@gmail.com>
2018-01-20 16:56:52 +0100

    Cookie compilance and style additions

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SessionStorage} from 'ngx-webstorage';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  @SessionStorage('wfnCookie', false)
+  cookiesAccepted: boolean;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  acceptCookies() {
+    this.cookiesAccepted = true;
+  }
 }
